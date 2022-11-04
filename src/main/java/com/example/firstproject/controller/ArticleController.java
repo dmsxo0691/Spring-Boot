bc296481dbba3d8091b5e5fcf85089f7fde74789
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-11-04 10:45:18 +0900

    2022. 11. 04 Lombok : 코드 개선

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,15 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //loging을 위한 롬복 어노테이션
 public class ArticleController {
     @Autowired // 스프링부트가 미리 생성해놓은 레퍼지토리 객체를 호출(DI:의존성 주입)
     private ArticleRepository articleRepository;
@@ -21,16 +24,29 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
 
-        System.out.println(form.toString());
+        //System.out.println(form.toString());
+        log.info(form.toString());
+
         //1. dto를 Entity 방식으로 변환
         Article article = form.toEntity();
+        log.info(article.toString());
 
         //2. Repository에게 Entity 를 DB로 저장하게 함
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
 
+    @GetMapping("/articles/{id}") //해당 url요청을 처리 선언
+    public String show(@PathVariable Long id){ // 해당 url에서 id 변수를 가져옴
+        log.info("id = " + id);
+        // 1. id로 데이터 호출
 
+        // 2. 호출한 데이터를 모델에 등록
+
+        // 3. 보여줄 페이지를 완성
+
+        return "";
+    }
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,23 +1,15 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
+//롬복-리팩터링 : 결과와 관계없이 코드의 구조를 개선
 public class ArticleForm {
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);

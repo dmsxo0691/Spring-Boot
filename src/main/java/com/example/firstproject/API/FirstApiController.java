a0ebbd0e4ec320b0384ffbacc87474fb47374924
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-11-15 10:45:47 +0900

    2022. 11. 15 Comment 작성 3

--- src/main/java/com/example/firstproject/API/FirstApiController.java
+++ src/main/java/com/example/firstproject/API/FirstApiController.java
@@ -11,3 +11,4 @@ public class FirstApiController {
         return "hello world";
     }
 }
+
---
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,40 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+    // article data도 가져와야 하므로 둘 다 불러옴
+
+    public List<CommentDto> comments(Long articleId) {
+        // 댓글 목록 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+        // Entity를 dto로 변환
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        // 비어있는 dtos(arrayList) 하나하나를 for문으로 채워줌
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            // 클래스 방식을 호출
+            // createCommentDto(); 생성 메소드를 만들어서 사용
+            dtos.add(dto);
+        }
+        // 반환
+        return dtos;
+    }
+
+
+}

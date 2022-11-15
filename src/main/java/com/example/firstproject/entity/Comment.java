431a0d76520d9f4e7ce9d8eee1078a0b9cebcf73
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-11-15 13:05:43 +0900

    2022. 11. 15 Comment 작성 4

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,4 +28,18 @@ public class Comment {
     @Column
     private String body;
 
+    public static Comment createComment(CommentDto dto, Article article) {
+        // 예외 처리
+        if(dto.getId() != null)
+            throw new IllegalArgumentException("댓글 생성 실패: 댓글의 id가 없어야 함");
+        if(dto.getArticleId() != article.getId())
+            throw new IllegalArgumentException("댓글 생성 실패: 게시글의 id가 잘못됐습니다");
+        // 엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+    }
 }
